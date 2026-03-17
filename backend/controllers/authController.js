f228a523f28a9efa409730bd71d063bd6f730612
Si <sdoukoure12@gmail.com>
2026-03-17 22:33:06 +0100

    Add authController for user registration and login
    This file contains the authentication controller with register and login functionalities, including JWT token generation and password comparison.

---
+++ backend/controllers/authController.js
@@ -0,0 +1,42 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
+
+exports.register = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ message: 'Utilisateur déjà existant' });
+
+    const user = await User.create({ email, password });
+    res.status(201).json({
+      _id: user._id,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user && (await user.comparePassword(password))) {
+      res.json({
+        _id: user._id,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: 'Email ou mot de passe incorrect' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
